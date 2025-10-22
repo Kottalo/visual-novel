7c148276e347130669d6b604e956b1968624459e
DITA\kotta <kottaloflotenspieler@gmail.com>
2025-10-23 03:47:21 +0800

    Checkpoint

--- game.gd
+++ game.gd
@@ -1,4 +1,11 @@
 extends Control
 
+@export var main_dialogue: Resource
+
 func _ready() -> void:
-	pass
+	DialogueManager.got_dialogue.connect(
+		func (line: DialogueLine):
+			print(line)
+	)
+	
+	DialogueManager.show_dialogue_balloon(main_dialogue, "start")
