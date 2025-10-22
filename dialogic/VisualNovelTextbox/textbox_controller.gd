8d1143daf06e53237aceaf10733d50b52e677543
DITA\kotta <kottaloflotenspieler@gmail.com>
2025-10-22 23:34:14 +0800

    Checkpoint

--- dialogic/VisualNovelTextbox/textbox_controller.gd
+++ dialogic/VisualNovelTextbox/textbox_controller.gd
@@ -3,6 +3,6 @@ extends Node
 @export var sizer: Control
 @export var textbox_layer: Control
 
-func _ready() -> void:
-	sizer.position = Vector2()
-	sizer.size = textbox_layer.size
+#func _process(delta: float) -> void:
+	#sizer.global_position = Vector2()
+	#sizer.size = 
