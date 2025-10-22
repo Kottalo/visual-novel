620307a637b6a88a0f3cf0e66df037796b1932ff
DITA\kotta <kottaloflotenspieler@gmail.com>
2025-10-23 00:01:02 +0800

    Checkpoint

--- dialogic/portraits/custom_layered_portrait.gd
+++ dialogic/portraits/custom_layered_portrait.gd
@@ -1,2 +1,11 @@
 @tool
 extends "res://addons/dialogic/Modules/LayeredPortrait/layered_portrait.gd"
+
+@export var main: Sprite2D
+@export var parts: Array[Node2D]
+
+func _ready() -> void:
+	for part in parts:
+		for item: Sprite2D in part.get_children():
+			item.region_enabled = true
+			item.region_rect = main.region_rect
