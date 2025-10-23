58fd83113315d58b12e42bf2809eb501eaa50bd8
DITA\kotta <kottaloflotenspieler@gmail.com>
2025-10-23 18:00:14 +0800

    Checkpoint

--- characters/character.gd
+++ characters/character.gd
@@ -4,10 +4,9 @@ extends Node2D
 @export var subviewport: SubViewport
 @export var avatar_frame: Control
 
-var avatar_image:
-	get:
-		var atlas_texture = AtlasTexture.new()
-		atlas_texture.atlas = subviewport.get_texture()
-		atlas_texture.region = avatar_frame.get_rect()
-		
-		return atlas_texture
+func get_avatar_image() -> AtlasTexture:
+	var atlas_texture = AtlasTexture.new()
+	atlas_texture.atlas = subviewport.get_texture()
+	atlas_texture.region = avatar_frame.get_rect()
+	
+	return atlas_texture
---
+++ characters/character_data.gd
@@ -0,0 +1,8 @@
+class_name CharacterData
+extends Resource
+
+@export var body: String
+@export var eyebrows: String
+@export var eyes: String
+@export var mouth: String
+@export var additional: Dictionary[String, bool]
