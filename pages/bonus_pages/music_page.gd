fdde5d3ec924fe0945cbe6317ab20a8736783c59
DITA\kotta <kottaloflotenspieler@gmail.com>
2025-10-25 04:30:49 +0800

    Checkpoint

--- pages/bonus_pages/music_page.gd
+++ pages/bonus_pages/music_page.gd
@@ -2,18 +2,25 @@ extends Control
 
 const PlayStatus = AudioManager.PlayStatus
 
+@export var track_item_scene: PackedScene
 @export var play_progress_container: Control
 @export var play_progress_line: ColorRect
 @export var play_button: TextureButton
 @export var pause_button: TextureButton
 @export var next_button: TextureButton
 @export var previous_button: TextureButton
+@export var vbox_playlist: VBoxContainer
 
 var audio_player: AudioStreamPlayer2D:
 	get:
 		return AudioManager.audio_player
 
 func _ready() -> void:	
+	for music_data in AudioManager.playlist:
+		var track_item: TrackItem = track_item_scene.instantiate()
+		track_item.music_data = music_data
+		vbox_playlist.add_child(track_item)
+	
 	AudioManager.play_status_changed.connect(
 		func ():
 			play_button.visible = false
@@ -38,7 +45,6 @@ func _ready() -> void:
 	previous_button.pressed.connect(
 		func (): AudioManager.track_index -= 1
 	)
-	
 
 func _physics_process(delta: float) -> void:
 	play_progress_line.size.x = play_progress_container.size.x * \
--- prefabs/track_item/track_item.gd
+++ prefabs/track_item/track_item.gd
@@ -2,6 +2,12 @@ class_name TrackItem
 extends TextureButton
 
 @export var drag_filter: DragFilter
+@export var label_title: Label
+
+var music_data: MusicData:
+	set(value):
+		music_data = value
+		label_title.text = music_data.title
 
 func _ready() -> void:
 	drag_filter.execute.connect(
