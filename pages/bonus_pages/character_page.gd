4a4e4598ae788d33c4b29febfde31147cb55dd83
DITA\kotta <kottaloflotenspieler@gmail.com>
2025-10-26 19:15:29 +0800

    Checkpoint

--- pages/bonus_pages/character_page.gd
+++ pages/bonus_pages/character_page.gd
@@ -1,7 +1,15 @@
 class_name CharacterPage
 extends Control
 
+@export var label_character_name: Label
+@export var background_pool: Control
 @export var character_pool: Node2D
+
+var background_index: int:
+	set(value):
+		background_index = value
+		for child: Control in background_pool.get_children():
+			child.visible = child.get_index() == background_index
 
 var current_character: Character:
 	get:
@@ -11,8 +19,10 @@ func _ready() -> void:
 	Main.character_selection_index_changed.connect(update_characters)
 	
 	update_characters()
+	background_index = 0
 
 func update_characters() -> void:
 	for child: Node2D in character_pool.get_children():
 		child.visible = false
 	current_character.visible = true
+	label_character_name.text = current_character.name
