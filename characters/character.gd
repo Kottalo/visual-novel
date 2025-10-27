423283ed2bd85258006268856a3f0eb06edcaf4e
DITA\kotta <kottaloflotenspieler@gmail.com>
2025-10-27 19:23:18 +0800

    Checkpoint

--- characters/character.gd
+++ characters/character.gd
@@ -11,6 +11,8 @@ extends Node2D
 var body_part_dict: Dictionary[String, AnimatedSprite2D]
 var character_image: Node2D
 
+var stage_page: StagePage = Pages.stage
+
 var character_data: CharacterData:
 	get:
 		return
@@ -32,7 +34,7 @@ func _ready() -> void:
 	DialogueManager.got_dialogue.connect(
 		func (line: DialogueLine):
 			if line.character == self.name:
-				Main.game.balloon.avatar.texture = get_avatar_image()
+				stage_page.balloon.avatar.texture = get_avatar_image()
 	)
 
 func update_bonus_part_index(part_name: String, increment: int) -> void:
@@ -64,8 +66,8 @@ func FadeIn(position_name: String, duration: float = 0) -> void:
 	character_sprite.position.y -= character_sprite.get_rect().size.y / 2 \
 	* character_image.scale.y
 	character_image.modulate.a = 0
-	Main.game.character_image_pool.add_child(character_image)
-	character_image.global_position = Main.game.get_position_by_name(position_name)
+	stage_page.character_image_pool.add_child(character_image)
+	character_image.global_position = stage_page.get_position_by_name(position_name)
 	await create_tween().tween_property(character_image, "modulate:a", 1, duration).finished
 
 func FadeOut(duration: float = 0) -> void:
@@ -73,7 +75,7 @@ func FadeOut(duration: float = 0) -> void:
 	character_image.queue_free()
 
 func MoveTo(position_name: String, duration: float = 0.5) -> void:
-	var target_position: Vector2 = Main.game.get_position_by_name(position_name)
+	var target_position: Vector2 = stage_page.get_position_by_name(position_name)
 	await create_tween().tween_property(character_image, "global_position", target_position, duration).finished
 
 # Example: SetParts("Body:校服,Eye:悲伤")
--- pages/stage_page.gd
+++ pages/stage_page.gd
@@ -1,2 +1,20 @@
 class_name StagePage
 extends Control
+
+@export var main_dialogue: Resource
+@export var hbox_positions: HBoxContainer
+@export var character_image_pool: Node2D
+
+var balloon: DialogueBalloon
+
+func _ready() -> void:
+	Pages.stage = self
+	
+	balloon = DialogueManager.show_dialogue_balloon(main_dialogue, "start", [Main])
+	
+	pass
+
+func get_position_by_name(position_name: String) -> Vector2:
+	var position_node: Control = hbox_positions.get_node(position_name + "/CenterPoint")
+	
+	return position_node.global_position
