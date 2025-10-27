ba498c791731450c39eeeb8c7e38e71902d36805
DITA\kotta <kottaloflotenspieler@gmail.com>
2025-10-28 04:02:23 +0800

    Checkpoint

--- pages/travel_page.gd
+++ pages/travel_page.gd
@@ -4,6 +4,8 @@ extends Control
 @export var vbox_selections: VBoxContainer
 @export var place_selection: Control
 @export var texture_button_confirm: TextureButton
+@export var label_place_title: Label
+@export var rich_label_place_description: RichTextLabel
 
 @onready var original_y: float = vbox_selections.global_position.y
 
@@ -14,9 +16,15 @@ var start_y: float
 var selected_index: int:
 	set(value):
 		var last_index = selected_index
-		selected_index = value
 		
-		var index_difference = last_index - selected_index
+		var index = value
+		var background_count = Stage.background_data_pool.size()
+		if index < 0: index = background_count - 1
+		index %= background_count
+		
+		selected_index = index
+		
+		var index_difference = last_index - value
 		var target_y = original_y + (place_selection.size.y * index_difference)
 		sliding = true
 		await create_tween().tween_property(
@@ -57,9 +65,9 @@ func _input(event: InputEvent) -> void:
 		
 		if event.pressed and not sliding:
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP :
-				selected_index += 1
-			elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 				selected_index -= 1
+			elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				selected_index += 1
 	
 	var y_difference: float
 	if button_pressed:
@@ -82,3 +90,7 @@ func update() -> void:
 		selection.texture_rect_image.texture = \
 			Stage.background_data_pool[target_index].texture
 	vbox_selections.global_position.y = original_y
+	
+	var selected_background = Stage.background_data_pool[selected_index]
+	label_place_title.text = selected_background.title
+	rich_label_place_description.text = selected_background.description
