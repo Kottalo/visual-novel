3f3c6dee2f11d994d72626e61a4698e440e1df56
DITA\kotta <kottaloflotenspieler@gmail.com>
2025-10-31 19:20:51 +0800

    Checkpoint

--- pages/pages.gd
+++ pages/pages.gd
@@ -20,31 +20,19 @@ var current_page: Node:
 		current_page = value
 		for page in Main.game.main_pages:
 			page.visible = page == current_page
+		update_page_bgm()
 
-#func _ready() -> void:
-	#while pages_with_bgm.size() != 5:
-		#await get_tree().process_frame
-	#print("ipda")
-	#for page in pages_with_bgm:
-		#page.visibility_changed.connect(
-			#func ():
-				#if stage:
-					#if stage.visible:
-						#return
-				#update_page_bgm(page)
-		#)
-	#
-	#update_page_bgm(main_menu)
+func _ready() -> void:
+	while not main_menu: await get_tree().process_frame
+	current_page = main_menu
 
-func update_page_bgm(page: Control) -> void:
+func update_page_bgm() -> void:
 	var audio_player = AudioManager.audio_player_music
-	if page.visible:
-		if not audio_player.playing:
-			audio_player.stream = \
-				AudioManager.playlist[0].track
-			audio_player.playing = true
-		audio_player.stream_paused = false
-	else:
-		
+	
+	if not audio_player.playing:
+		audio_player.stream = \
+			AudioManager.playlist[0].track
+		audio_player.playing = true
+	
+	if current_page == stage or current_page == music:
 		audio_player.playing = false
-		audio_player.stream_paused = true
