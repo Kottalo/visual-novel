582343fff7794fced96d5a81d5ec6b60df8660f6
DITA\kotta <kottaloflotenspieler@gmail.com>
2026-01-12 16:44:15 +0800

    Checkpoint

---
+++ data/_models/background_data.gd
@@ -0,0 +1,7 @@
+class_name BackgroundData
+extends Resource
+
+@export var title: String
+@export_multiline var description: String
+@export var texture: Texture2D
+@export var variations: Dictionary[String, Texture2D]
--- scripts/background_data.gd
+++
@@ -1,6 +0,0 @@
-class_name BackgroundData
-extends Resource
-
-@export var title: String
-@export_multiline var description: String
-@export var texture: Texture2D
