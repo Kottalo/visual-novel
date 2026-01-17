649b7c1c76e8c38d7cfbeeb7aa3ef2f3d662c3c1
DITA\kotta <kottaloflotenspieler@gmail.com>
2026-01-17 23:42:36 +0800

    Checkpoint

--- pages/phone_page.gd
+++ pages/phone_page.gd
@@ -1,11 +1,16 @@
 class_name PhonePage
 extends CanvasLayer
 
+@export var phone_background: Texture2D
+@export var chat_background: Texture2D
+
 @export var background: Control
 @export var phone_icon_message: PhoneIcon
 @export var phone_icon_photo: PhoneIcon
 @export var phone_icon_music: PhoneIcon
 @export var phone_icon_book: PhoneIcon
+
+@export var message_text_pool: Control
 
 func _ready() -> void:
 	background.gui_input.connect(
---
+++ prefabs/chat_message/chat_message.gd
@@ -0,0 +1,43 @@
+@tool
+class_name ChatMessage
+extends HBoxContainer
+
+@export_tool_button("Reset") var reset_button = reset
+
+@export var sender_type: Enums.SenderType:
+	set(value):
+		sender_type = value
+		var is_self = sender_type == Enums.SenderType.SELF
+		size_flags_horizontal = SIZE_SHRINK_END if is_self else SIZE_SHRINK_BEGIN
+		avatar_left.modulate.a = 1 if not is_self else 0
+		avatar_right.modulate.a = 1 if is_self else 0
+		message_text.self_modulate = Color.WHITE if is_self else Color.BLACK
+		bubble.self_modulate = self_bubble_color if is_self else Color.WHITE
+		alignment = BoxContainer.ALIGNMENT_END if is_self else \
+			BoxContainer.ALIGNMENT_BEGIN
+
+@export var self_bubble_color: Color
+@export var avatar_left: TextureRect
+@export var avatar_right: TextureRect
+@export var bubble: PanelContainer
+@export var message_text: RichTextLabel
+
+func _ready() -> void:
+	reset()
+	message_text.resized.connect(
+		func ():
+			var parent: Control = get_parent()
+			if size.x >= parent.size.x:
+				size_flags_horizontal = SIZE_EXPAND_FILL
+				bubble.size_flags_horizontal = SIZE_EXPAND_FILL
+				message_text.autowrap_mode = TextServer.AUTOWRAP_ARBITRARY
+			print("Resized")
+			print({
+				"size.x": size.x,
+				"parent.size.x": parent.size.x,
+			})
+	)
+
+func reset() -> void:
+	bubble.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
+	message_text.autowrap_mode = TextServer.AUTOWRAP_OFF
