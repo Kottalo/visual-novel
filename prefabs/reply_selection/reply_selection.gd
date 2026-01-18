019584fb8c70a0d792d89b422b1735b451fe9b6c
DITA\kotta <kottaloflotenspieler@gmail.com>
2026-01-18 21:12:28 +0800

    Checkpoint

--- prefabs/reply_selection/reply_selection.gd
+++ prefabs/reply_selection/reply_selection.gd
@@ -1,2 +1,24 @@
+@tool
 class_name ReplySelection
 extends PanelContainer
+
+@export var reply_text: RichTextLabel
+
+@export var hover_shade: Control
+@export var select_shade: Control
+@export var hovered: bool:
+	set(value):
+		hovered = value
+		hover_shade.visible = hovered
+@export var selected: bool:
+	set(value):
+		selected = value
+		select_shade.visible = selected
+
+func _ready() -> void:
+	mouse_entered.connect(
+		func (): hovered = true
+	)
+	mouse_exited.connect(
+		func (): hovered = false
+	)
