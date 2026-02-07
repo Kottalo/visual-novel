7345cfca39c5a521bfdb1cc56b412d5e008cfc03
Kottalo <kottaloflotenspieler@gmail.com>
2026-02-07 18:45:21 +0800

    Checkpoint

--- pages/stage_page/stage_page.gd
+++ pages/stage_page/stage_page.gd
@@ -2,6 +2,8 @@ class_name StagePage
 extends CanvasLayer
 ## A basic dialogue balloon for use with Dialogue Manager.
 
+signal next_line
+
 @export var chapters: Array[DialogueResource]
 var chapters_dict: Dictionary[String, DialogueResource]
 
@@ -48,16 +50,19 @@ func get_position_by_name(position_name: String) -> Vector2:
 	
 	return position_node.global_position
 
+var finish_pause: float = 2
 var char_per_sec: float = 12
 var text_progress: float:
 	set(value):
 		text_progress = value
 		dialogue_label.visible_characters = int(text_progress)
+var total_text_progress: int:
+	get: return dialogue_label.text.length()
 
 func _process(delta: float) -> void:
 	if not dialogue_line: return
-	print(delta)
-	if text_progress < dialogue_label.text.length():
+	
+	if text_progress < total_text_progress:
 		text_progress += char_per_sec * delta
 		
 
@@ -66,8 +71,20 @@ var dialogue_line: DialogueLine:
 	set(value):
 		if value:
 			dialogue_line = value
-			dialogue_label.text = dialogue_line.text
-			text_progress = 0
+			print(dialogue_line)
+			process_line()
+
+func process_line() -> void:
+	dialogue_label.text = dialogue_line.text
+	text_progress = 0
+	while text_progress < total_text_progress:
+		await get_tree().process_frame
+	if autoplay:
+		await get_tree().create_timer(finish_pause).timeout
+	else:
+		await next_line
+	
+	dialogue_line = await dialogue.get_next_dialogue_line(dialogue_line.next_id, [self, Stage])
 
 func _ready() -> void:
 	dialogue_label.visible_characters = 0
@@ -75,7 +92,12 @@ func _ready() -> void:
 		var chapter_name = chapter.resource_path.get_file()[0]
 		chapters_dict[chapter_name] = chapter
 	
-	
+	dialogue_screen.gui_input.connect(
+		func (event: InputEvent):
+			if event is InputEventMouseButton:
+				if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+					next_line.emit()
+	)
 
 func start() -> void:
 	dialogue_line = await dialogue.get_next_dialogue_line("start", [self, Stage])
